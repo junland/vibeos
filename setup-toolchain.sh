167d30bccbcf5716c05b7498aa9f2a6efb6e71db
John Unland <junland.foss@gmail.com>
2026-02-18 13:09:41 -0600

    Fix script to reference relocate-sdk.sh

--- setup-toolchain.sh
+++ setup-toolchain.sh
@@ -38,9 +38,9 @@ else
 fi
 
 # Make sure to run the toolchain's setup script if it exists
-if [ -f "$TOOLCHAIN_EXTRACT_DIR/relocate-sdk" ]; then
+if [ -f "$TOOLCHAIN_EXTRACT_DIR/relocate-sdk.sh" ]; then
 	echo "Running toolchain setup script..."
-	"$TOOLCHAIN_EXTRACT_DIR/relocate-sdk" "$TOOLCHAIN_EXTRACT_DIR" 
+	"$TOOLCHAIN_EXTRACT_DIR/relocate-sdk.sh" "$TOOLCHAIN_EXTRACT_DIR" 
 fi
 
 echo "Toolchain setup complete. Toolchain is located at $TOOLCHAIN_EXTRACT_DIR"
