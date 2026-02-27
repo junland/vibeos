371bd9cc079405330dd60170dd40b10bcd2efc93
John Unland <unlandj2012@gmail.com>
2026-02-27 15:40:40 +0000

    Update PATCH_VER to 2.8 for patch installation step

--- steps/patch_step.sh
+++ steps/patch_step.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Patch Step - Build and install patch
 
-PATCH_VER="2.7.6"
+PATCH_VER="2.8"
 
 step_patch() {
 	extract_file "${SOURCES_DIR}/patch-${PATCH_VER}.tar.xz" "${WORK_DIR}/patch-${PATCH_VER}"
