940a7f287383167bc354296070cd4cd00dd3be68
John Unland <unlandj2012@gmail.com>
2026-06-14 20:03:46 -0500

    Remove unnecessary --with-headers option from glibc configuration

--- steps/glibc_step.sh
+++ steps/glibc_step.sh
@@ -47,7 +47,6 @@ step_glibc() {
 		--prefix=/usr \
 		--host="${LFS_TGT}" \
 		--build="$(../scripts/config.guess)" \
-		--with-headers="${TARGET_ROOTFS_DIR}/usr/include" \
 		--enable-kernel=5.4 \
 		--disable-nscd \
 		libc_cv_slibdir=/usr/lib
